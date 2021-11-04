76b71daf0785035b7f030d1c1a14daafabac745f
Nour Shaheen <mailto:nshaheen@youxel.com>
2021-11-04 12:38:36 +0200

    init commit

---
+++ shopping-back/src/main/java/com/shopping/ShoppingBackApplication.java
@@ -0,0 +1,13 @@
+package com.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBackApplication.class, args);
+	}
+
+}
